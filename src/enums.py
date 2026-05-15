2738b89abb9e49302bd85a96881397e4cd987ed0
sunyesle <sunyesle@gmail.com>
2026-05-15 23:08:37 +0900

    Order, OrderItem 모델 정의

--- src/enums.py
+++ src/enums.py
@@ -4,3 +4,11 @@ from enum import Enum
 class UserRole(str, Enum):
     user = "user"
     admin = "admin"
+
+class OrderStatus(str, Enum):
+    pending = "pending"        # 결제 대기
+    paid = "paid"              # 결제 완료
+    preparing = "preparing"    # 배송 준비
+    shipping = "shipping"      # 배송 중
+    delivered = "delivered"    # 배송 완료
+    cancelled = "cancelled"    # 취소
